9237be53c6b90ccb271da748a4d6636429207a3f
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2016-08-11 11:40:09 -0400

    Added host to Session
    host is an attribute; request only have to specify enpoint after api/

--- onsitesession.py
+++ onsitesession.py
@@ -5,9 +5,18 @@ class OnSiteSession(requests.Session):
     """Creates a Session to send requests to the OnSite public API.
 
     Stores private App ID, user agent, username and password."""
-    def __init__(self, pappid, user_agent, username, password):
+    def __init__(self, host, pappid, user_agent, username, password):
         super(OnSiteSession, self).__init__()
+        self.host = host
         self.headers = {'X_PAPPID': pappid,
                         'User-Agent': user_agent}
         self.auth = (username, password)
         self.verify = False
+
+    def request(self, method, endpoint, **kwargs):
+        endpoint = endpoint.lstrip('/')
+        if endpoint[-1] != '/':
+            endpoint += '/'
+        url = "https://{0}/api/{1}".format(self.host, endpoint)
+        r = super(OnSiteSession, self).request(method, url, **kwargs)
+        return r
