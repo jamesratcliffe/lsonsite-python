871e4539cb9567b2c3b041b8d10b7f7004f584f8
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2016-08-12 12:23:21 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name=lsonsite,
+    version='1.0',
+    description='Requests for Lightspeed OnSite',
+    url='',
+    author='James Ratcliffe',
+    license='OSL',
+    packages=['lsonsite'],
+    zip_safe=False)
